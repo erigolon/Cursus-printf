b3a31879aa5d09f9fd8880b0db5ccce0fc3d2a36
Esteban Rigolon <erigolon@c2r4s2.42malaga.com>
2023-01-12 16:10:36 +0100

    %s finalizado

--- ft_check_percentage.c
+++ ft_check_percentage.c
@@ -6,7 +6,7 @@
 /*   By: erigolon <erigolon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 12:38:57 by erigolon          #+#    #+#             */
-/*   Updated: 2023/01/12 15:56:56 by erigolon         ###   ########.fr       */
+/*   Updated: 2023/01/12 16:07:50 by erigolon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_check_percentage(char convert, va_list args, int count)
 	if (convert == 'c')
 		count += ft_putcharf(va_arg(args, int));
 	else if (convert == 's')
-		count += ft_putstrf(va_arg(args, char *), count);
+		count += ft_putstrf(va_arg(args, char *));
 	else if (convert == '%')
 		count += ft_putcharf('%');
 	return (count);
--- ft_putstrf.c
+++ ft_putstrf.c
@@ -6,13 +6,13 @@
 /*   By: erigolon <erigolon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:44:40 by erigolon          #+#    #+#             */
-/*   Updated: 2023/01/12 16:05:49 by erigolon         ###   ########.fr       */
+/*   Updated: 2023/01/12 16:10:13 by erigolon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-/*
-int	ft_putstrf(char *str, int count)
+
+int	ft_putstrf(char *str)
 {
 	int	i;	
 
@@ -22,28 +22,10 @@ int	ft_putstrf(char *str, int count)
 		return (6);
 	}
 	i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		count += ft_putcharf(str[i]);
+		ft_putcharf(str[i]);
 		i++;
 	}
-	return (count);
-}
-*/
-int	ft_putstrf(char *str, int count)
-{
-	int	i;	
-
-	if (!str)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	i = 0;
-	while (str[i] != '\0')
-	{
-		count += ft_putcharf(str[i]);
-		i++;
-	}
-	return (count);
+	return (i);
 }
